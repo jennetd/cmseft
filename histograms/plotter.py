399ef373158e0aaec5c90cd75c12c26f90b825bc
Jennet Dickinson <dickinson.jennet@gmail.com>
2024-12-12 14:18:46 -0600

    jennet's coffea hists

--- histograms/plotter.py
+++ histograms/plotter.py
@@ -10,26 +10,52 @@ from topcoffea.modules.histEFT import HistEFT
 
 WCPT_EXAMPLES = {
     "nonsm" : {
-        "cHtbIm" : 1.5,
-        "cHtbRe" : 1.5,
-        "ctGIm"  : 1.5, 
-        "cHt"    : 1.5, 
-        "ctGRe"  : 1.5, 
-        "ctWRe"  : 1.5, 
-        "ctWIm"  : 1.5, 
-        "ctBIm"  : 1.5, 
-        "ctBRe"  : 1.5,
+        'cHudIm' : 1.0,
+        'cuBIm' : 1.0, 
+        'cHWBtil' : 1.0,
+        'cHbox' : 1.0,
+        'cHd' : 1.0,
+        'cHDD' : 1.0,
+        'cuBRe' : 1.0,
+        'cuWIm' : 1.0,
+        'cHW' : 1.0,
+        'cHBtil' : 1.0,
+        'cHu' : 1.0,
+        'cdWIm' : 1.0,
+        'cHWB' : 1.0,
+        'cdBIm' : 1.0,
+        'cHWtil' : 1.0,
+        'cuWRe' : 1.0,
+        'cdWRe' : 1.0,
+        'cdBRe' : 1.0,
+        'cHB' : 1.0,
+        'cHj1' : 1.0,
+        'cHudRe' : 1.0,
+        'cHj3' : 1.0
     },
     "sm" : {
-        "cHtbIm" : 0.0,
-        "cHtbRe" : 0.0,
-        "ctGIm"  : 0.0, 
-        "cHt"    : 0.0, 
-        "ctGRe"  : 0.0, 
-        "ctWRe"  : 0.0, 
-        "ctWIm"  : 0.0, 
-        "ctBIm"  : 0.0, 
-        "ctBRe"  : 0.0,
+        'cHudIm' : 0.0,
+        'cuBIm' : 0.0,
+        'cHWBtil' : 0.0,
+        'cHbox' : 0.0,
+        'cHd' : 0.0,
+        'cHDD' : 0.0,
+        'cuBRe' : 0.0,
+        'cuWIm' : 0.0,
+        'cHW' : 0.0,
+        'cHBtil' : 0.0,
+        'cHu' : 0.0,
+        'cdWIm' : 0.0,
+        'cHWB' : 0.0,
+        'cdBIm' : 0.0,
+        'cHWtil' : 0.0,
+        'cuWRe' : 0.0,
+        'cdWRe' : 0.0,
+        'cdBRe' : 0.0,
+        'cHB' : 0.0,
+        'cHj1' : 0.0,
+        'cHudRe' : 0.0,
+        'cHj3' : 0.0
     }
 }
 
@@ -58,7 +84,7 @@ def main():
 
     # Grab the one we want to plot
     #variable = "j0pt"
-    variable = "ht"
+    variable = "hpt"
     histo = hin_dict[variable]
 
     # Print some values
@@ -70,7 +96,7 @@ def main():
         wc_pt = WCPT_EXAMPLES[wcpt_name]
         histo_to_plot = histo.as_hist(wc_pt)
         fig = make_single_fig(histo_to_plot)
-        title = f"ttbar_{wcpt_name}.png"
+        title = f"vbf_{wcpt_name}.png"
         fig.savefig(os.path.join(".",title))
 
 
---
+++ histograms/vbfprocessor.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+import numpy as np
+import awkward as ak
+np.seterr(divide='ignore', invalid='ignore', over='ignore')
+from coffea import processor
+from coffea.analysis_tools import PackedSelection
+
+# silence warnings due to using NanoGEN instead of full NanoAOD
+from coffea.nanoevents import NanoAODSchema
+NanoAODSchema.warn_missing_crossrefs = False
+
+import hist
+
+# Get the lumi for the given year
+def get_lumi(year):
+    lumi_dict = {
+        "2016APV": 19.52,
+        "2016": 16.81,
+        "2017": 41.48,
+        "2018": 59.83
+    }
+    if year not in lumi_dict.keys():
+        raise Exception(f"(ERROR: Unknown year \"{year}\".")
+    else:
+        return(lumi_dict[year])
+
+# Clean objects
+def is_clean(obj_A, obj_B, drmin=0.4):
+    objB_near, objB_DR = obj_A.nearest(obj_B, return_metric=True)
+    mask = ak.fill_none(objB_DR > drmin, True)
+    return (mask)
+
+# Main analysis processor
+class AnalysisProcessor(processor.ProcessorABC):
+    def __init__(self, samples, wc_names_lst=[]):
+        self._samples = samples
+        self._wc_names_lst = wc_names_lst
+        
+        # Create the histograms with new scikit hist
+        self._histo_dict = {
+            "hpt": hist.Hist(
+                #hist.axis.StrCategory([], growth=True, name="process"),
+                hist.axis.Regular(name="hpt",label="Higgs $p_{T}$ (GeV)", bins=100, start=0, stop=1000, flow=True),
+                #wc_names = self._wc_names_lst,
+                label="Events",
+            ),
+            "q1pt": hist.Hist(
+                #hist.axis.StrCategory([], growth=True, name="process"),
+                hist.axis.Regular(name="q1pt",label="Quark 1 $p_{T}$ (GeV)", bins=100, start=0, stop=1000, flow=True),
+                #wc_names = self._wc_names_lst,
+                label="Events",
+            ),
+            "q2pt": hist.Hist(
+                #hist.axis.StrCategory([], growth=True, name="process"),
+                hist.axis.Regular(name="q2pt",label="Quark 2 $p_{T}$ (GeV)", bins=100, start=0, stop=1000, flow=True),
+                #wc_names = self._wc_names_lst,
+                label="Events",
+            ),
+            "mqq": hist.Hist(
+                #hist.axis.StrCategory([], growth=True, name="process"),
+                hist.axis.Regular(name="mqq",label="Higgs $p_{T}$ (GeV)", bins=100, start=0, stop=5000, flow=True),
+                #wc_names = self._wc_names_lst,
+                label="Events",
+            ),
+            "detaqq": hist.Hist(
+                #hist.axis.StrCategory([], growth=True, name="process"),
+                hist.axis.Regular(name="detaqq",label="$\Delta\eta_{qq}$", bins=100, start=-6, stop=6, flow=True),
+                #wc_names = self._wc_names_lst,
+                label="Events",
+            ),
+            "dphiqq": hist.Hist(
+                #hist.axis.StrCategory([], growth=True, name="process"),
+                hist.axis.Regular(name="dphiqq",label="$\Delta\phi_{qq}$", bins=100, start=-3.15, stop=3.15, flow=True),
+                #wc_names = self._wc_names_lst,
+                label="Events",
+            ),
+            "SumOfWeights": processor.value_accumulator(float),
+            "EventCount": processor.value_accumulator(int),
+        }
+
+
+    @property
+    def columns(self):
+        return self._columns
+
+    def process(self, events):
+
+        # Dataset parameters
+        dataset = events.metadata['dataset']
+        hist_axis_name = self._samples[dataset]["histAxisName"]
+        #year   = self._samples[dataset]['year']
+        #xsec   = self._samples[dataset]['xsec']
+        #sow    = self._samples[dataset]['nSumOfWeights' ]
+
+        # Extract the EFT quadratic coefficients and optionally use them to calculate the coefficients on the w**2 quartic function
+        # eft_coeffs is never Jagged so convert immediately to numpy for ease of use.
+        #eft_coeffs = ak.to_numpy(events['EFTfitCoefficients']) if hasattr(events, "EFTfitCoefficients") else None
+
+        # Initialize objects
+        genpart = events.GenPart
+        is_final_mask = genpart.hasFlags(["fromHardProcess","isLastCopy"])
+
+        outgoing = events.LHEPart[events.LHEPart.status==1]
+        higgs = ak.firsts(outgoing[outgoing.pdgId == 25])
+        quarks = outgoing[outgoing.pdgId<=6]
+
+        q1 = ak.firsts(quarks[:,0:1])
+        q2 = ak.firsts(quarks[:,1:2])
+
+        ######## Jet selection  ########
+        #jets = events.GenJet
+        #jets = jets[(jets.pt>30) & (abs(jets.eta)<2.5)]
+        #j0 = jets[ak.argmax(jets.pt,axis=-1,keepdims=True)]
+
+        ######## Event selections ########
+
+        #njets = ak.num(jets)
+        #at_least_one_jet = ak.fill_none(njets>=1,False)
+        #selections = PackedSelection()
+        #selections.add('1j', at_least_one_jet)
+
+        ######## Normalization ########
+
+        # Normalize by (xsec/sow)
+        #lumi = 1000.0*get_lumi(year)
+        #norm = (xsec/sow)*lumi
+        #wgts = norm*np.ones_like(events['event'])
+        wgts = events.genWeight
+
+        #print(lumi)
+        ######## Fill histos ########
+
+        hout = self._histo_dict
+
+        variables_to_fill = {
+            "hpt" : higgs.pt,
+            "q1pt" : q1.pt,
+            "q2pt" : q2.pt,
+            "mqq" : (q1+q2).mass,
+            "detaqq" : q1.eta - q2.eta,
+            "dphiqq" : q1.delta_phi(q2)
+        }
+
+        #event_selection_mask = selections.all("2j")
+
+        for var_name, var_values in variables_to_fill.items():
+
+            fill_info = {
+                var_name    : var_values,#[event_selection_mask],
+                #"process"   : hist_axis_name,
+                "weight"    : wgts,#[event_selection_mask],
+                #"eft_coeff" : eft_coeffs,#[event_selection_mask],
+            }
+
+            hout[var_name].fill(**fill_info)
+
+        hout["SumOfWeights"] = np.sum(wgts)
+        hout["EventCount"] = len(events)
+
+        return hout
+
+    def postprocess(self, accumulator):
+        return accumulator
+
